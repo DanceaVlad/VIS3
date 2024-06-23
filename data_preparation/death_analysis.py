8d513ae7e7c68ebff923c3132a9e8a58f790ca2a
Sunerloger <106279641+Sunerloger@users.noreply.github.com>
2024-06-24 00:20:25 +0200

    refac: changed folder and file name

--- data_preparation/death_analysis.py
+++ data_preparation/death_analysis.py
@@ -52,7 +52,7 @@ print(df_deaths.head())
 for col in df_deaths.columns[1:]:
     df_to_export = df_deaths[['Datum', col]]
     filename = convert_umlauts(f'deaths_per_1000_people_cumulative_{col}.csv')
-    df_to_export.to_csv(f'data/cleaned_jsons/{filename}', index=False, sep=';', quotechar='"', quoting=csv.QUOTE_ALL)
+    df_to_export.to_csv(f'data/cleaned_csvs/{filename}', index=False, sep=';', quotechar='"', quoting=csv.QUOTE_ALL)
 
 print("Done exporting files")
 
